ca305d921451c459d769c897ed06f349f1cb62f5
yluzhou <90240124+yluzhou@users.noreply.github.com>
2026-09-01 16:07:30 +0800

    Update ChIP_seq.sh

--- ChIP_seq.sh
+++ ChIP_seq.sh
@@ -1,12 +1,20 @@
 # ChIP-seq analysis pipeline
 
-trim_galore -q 25 --phred33 --length 36 -e 0.1 --stringency 4 -o  trim/ ${fq1}
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+gunzip -c hg38.fa.gz > hg38.fa
+bowtie2-build --threads 10 -f hg38.fa hg38
+index="hg38"
+
+trim_galore -q 25 --phred33 --length 36 -e 0.1 --stringency 4 -o  trim/ ${raw_fq}
+clean_fq="trim/${sample}_trimmed.fq.gz"
 
 # alignment
-bowtie2 -p 5 -x ${index} -U $id | samtools sort -O bam -@ 5 -o ./${sample}.bam
+bowtie2 -p 5 -x ${index} -U ${clean_fq} | samtools sort -O bam -@ 8 -o ${sample}.bam
+samtools index ${sample}.bam
 
 # remove PCR duplicate
-sambamba markdup -r -p -t 12 $id $(basename -s .bam $id).rmdup.bam
+sambamba markdup -r -p -t 8 ${sample}.bam ${sample}.rmdup.bam
+samtools index ${sample}.rmdup.bam
 
 # filter blacklist
 bedtools intersect -v -a ${sample}.rmdup.rmchrM.bam -b hg38.blacklist.bed > ${sample}.blacklist_filtered.last.bam
@@ -14,5 +22,5 @@ samtools index ${sample}.blacklist_filtered.last.bam
 bedtools bamtobed -i ${sample}.blacklist_filtered.last.bam > ${sample}.last.bed
 
 # call peaks
-macs3 callpeak -t ChIP.bam -c Control.bam -f BAMPE -n ${name} -g hs --outdir peaks/ -q 0.05 --keep-dup all
+macs3 callpeak -t ChIP.bam -c Control.bam -f BAM -n ${name} -g hs --outdir peaks/ -q 0.05 --keep-dup all
 
