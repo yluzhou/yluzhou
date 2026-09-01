7a97e3ef24d0e120dbc947ab2aab3fafb48fdd60
yluzhou <90240124+yluzhou@users.noreply.github.com>
2026-09-01 14:01:12 +0800

    Create ATAC_seq.sh

---
+++ ATAC_seq.sh
@@ -0,0 +1,28 @@
+# ATAC-seq analysis pipeline
+
+trim_galore --illumina -q 25 --phred33 --length 35 -e 0.1 --stringency 4 --paired -o trim/ ${fq1} ${fq2}
+clean_fq1="trim/${sample}_1_val_1.fq.gz"
+clean_fq2="trim/${sample}_2_val_2.fq.gz"
+
+# alignment
+bowtie2 -p 5 --very-sensitive -X 2000 -x ${index} -1 ${clean_fq1} -2 ${clean_fq2} | samtools sort -O bam -@ 5 -o - > ${sample}.bam
+samtools index ${sample}.bam
+samtools flagstat ${sample}.bam > ${sample}.raw.stat
+
+# remove PCR duplicates 
+sambamba markdup --overflow-list-size 600000  --tmpdir='./'  -r ${sample}.bam ${sample}.rmdup.bam
+samtools index ${sample}.rmdup.bam
+samtools idxstats ${sample}.rmdup.bam | cut -f 1 | grep -v -E 'chrM|chrMT|MT' | xargs samtools view -b -f 2 -q 30 -@ 5 ${sample}.rmdup.bam > ${sample}.rmdup.rmchrM.bam
+samtools index ${sample}.rmdup.rmchrM.bam
+bedtools bamtobed -i ${sample}.rmdup.rmchrM.bam  > ${sample}.bed
+
+# filter blacklist
+bedtools intersect -v -a ${sample}.rmdup.rmchrM.bam -b hg38.blacklist.bed > ${sample}.blacklist_filtered.last.bam
+samtools index ${sample}.blacklist_filtered.last.bam
+bedtools bamtobed -i ${sample}.blacklist_filtered.last.bam > ${sample}.last.bed
+
+# call peaks
+macs3  callpeak -t ${sample}.blacklist_filtered.last.bam -g hs -f BAMPE -n ${sample_name} --outdir ${out}/ -q 0.05 --keep-dup all
+
+# generate bigwig file for visualization
+bamCoverage --normalizeUsing RPKM -b ${sample}.blacklist_filtered.last.bam -o ${output}.bw
