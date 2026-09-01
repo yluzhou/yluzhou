eb28aba4e6db6ff18f808a6d8ef2a66f21630290
yluzhou <90240124+yluzhou@users.noreply.github.com>
2026-09-01 14:09:19 +0800

    Create ChIP_seq.sh

---
+++ ChIP_seq.sh
@@ -0,0 +1,15 @@
+# ChIP-seq analysis pipeline
+
+trim_galore -q 25 --phred33 --length 36 -e 0.1 --stringency 4 -o  trim/ ${fq1}
+
+# alignment
+bowtie2 -p 5 -x ${index} -U $id | samtools sort -O bam -@ 5 -o ./${sample}.bam
+
+# remove PCR duplicate
+sambamba markdup -r -p -t 12 $id $(basename -s .bam $id).rmdup.bam
+
+
+
+# call peaks
+macs3 callpeak -t ChIP.bam -c Control.bam -f BAMPE -n ${name} -g hs --outdir peaks/ -q 0.05 --keep-dup all
+
